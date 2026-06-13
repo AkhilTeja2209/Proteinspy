b539badab881633e4915ae53b3dabbc46c5f38e4
AkhilTeja2209 <mvsakhilteja@gmail.com>
2026-06-14 00:03:56 +0530

    chore: bump to v1.1.5, remove old release.yml

--- proteinspy_pkg/proteinspy/__init__.py
+++ proteinspy_pkg/proteinspy/__init__.py
@@ -22,7 +22,7 @@ from proteinspy.analysis.advanced import (
 from proteinspy.core.validator import validate_structure
 from proteinspy.io.writers import to_json, to_csv, write_output
 
-__version__ = "1.1.4"
+__version__ = "1.1.5"
 __author__ = "AkhilTeja2209"
 __license__ = "MIT"
 
